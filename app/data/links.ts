75186db47f42900a1cbbe566cc6643239acca1a9
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2026-07-18 07:52:00 +0800

    Update registration details

--- app/data/links.ts
+++ app/data/links.ts
@@ -1,4 +1,4 @@
-const REGISTRATION_LINK = ""
+const REGISTRATION_LINK = "https://forms.gle/YqB7zRxXj8VsdLaGA"
 const DISCORD_LINK = "https://discord.gg/AXCgugRbxS"
 
 export { REGISTRATION_LINK, DISCORD_LINK }
